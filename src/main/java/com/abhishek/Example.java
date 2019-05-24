40892c3b382afbf035c4c1f1f66483db68dcfb1b
AbhhishekMadepogu <50969821+AbhhishekMadepogu@users.noreply.github.com>
2019-05-24 19:24:29 -0400

    [Abhishek] array sum

---
+++ src/main/java/com/abhishek/Example.java
@@ -0,0 +1,29 @@
+package com.abhishek;
+
+public class Example
+{
+    public static void main (String [] args)
+    {
+        int x[]=new int[5];
+        int y[]={10,20,30,40,50,60,70,80,90,100};
+        x[0]=100;
+        x[1]=200;
+        x[2]=300;
+        x[3]=400;
+        x[4]=500;
+
+        Example main = new Example();
+        int s = main.sumofarray(x);
+        System.out.println(s);
+
+    }
+    public int sumofarray(int a[])
+    {
+        int sum=0;
+        for(int x: a)
+        {
+         sum = sum + x;
+        }
+        return(sum);
+    }
+}
