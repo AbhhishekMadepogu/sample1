293362558e1fd8597785ec26c8d387e5f3a5feeb
AbhhishekMadepogu <50969821+AbhhishekMadepogu@users.noreply.github.com>
2019-05-24 20:55:53 -0400

    [Abhishek] string functions

--- src/main/java/com/abhishek/Example.java
+++ src/main/java/com/abhishek/Example.java
@@ -1,33 +1,28 @@
 package com.abhishek;
 
-public class Example
-{
-    public static void main (String [] args)
-    {
-        int x[]=new int[5];
-        int y[]={10,20,30,40,50,60,70,80,90,100};
-        x[0]=100;
-        x[1]=200;
-        x[2]=300;
-        x[3]=400;
-        x[4]=500;
+public class Example {
+    public static void main(String[] args) {
+        int x[] = new int[5];
+        int y[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        x[0] = 100;
+        x[1] = 200;
+        x[2] = 300;
+        x[3] = 400;
+        x[4] = 500;
 
-        Example main = new Example();
-        int s = main.sumofarray(x);
-        System.out.println(s);
-        for(int i=0;i<x.length;i++)
-        {
-            System.out.println("X[]" + x[i]);
-        }
+        Example day = new Example();
+
+        day.handlestring();
 
     }
-    public int sumofarray(int a[])
+    public void handlestring()
     {
-        int sum=0;
-        for(int x: a)
-        {
-         sum = sum + x;
-        }
-        return(sum);
+        String s1="hello world";
+        System.out.println(s1);
+        s1=s1.concat(" testing");
+        System.out.println(s1);
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.endsWith("ing"));
+        System.out.println(s1.toUpperCase());
     }
 }
